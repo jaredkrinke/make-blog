b4b8fbb0a9f4a209b2f66d49e7c12a467b19918f
Jared Krinke <jared.krinke@gmail.com>
2023-03-29 21:54:42 -0700

    Initial front matter parsing

---
+++ process.ts
@@ -0,0 +1,19 @@
+import { parse } from "https://deno.land/std@0.178.0/encoding/yaml.ts";
+
+const processors: { [command: string]: (paths: string[]) => Promise<void> } = {
+	split: async (paths) => {
+		const frontMatterPattern = /^---\r?\n(.*?)\r?\n---(\r?\n|$)/ms;
+		const [pathInput, pathOutputMetadata, pathOutputContent] = paths;
+		const text = await Deno.readTextFile(pathInput);
+		const matches = frontMatterPattern.exec(text);
+		const hasFrontMatter = !!matches;
+		const metadata = hasFrontMatter ? parse(matches[1]) : {};
+		const content = hasFrontMatter ? text.slice(matches[0].length) : text;
+
+		await Deno.writeTextFile(pathOutputMetadata, JSON.stringify(metadata));
+		await Deno.writeTextFile(pathOutputContent, content);
+	},
+} as const;
+
+await processors[Deno.args[0]](Deno.args.slice(1));
+
